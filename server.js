e8ec40cb0f29a586f9ff55bfba52799ba58285bd
Samyak Jain <samyakjain7002@gmail.com>
2026-06-10 00:22:12 +0530

    fix: resolve express 5 wildcard routing error

--- server.js
+++ server.js
@@ -85,7 +85,7 @@ app.post('/api/insights', async (req, res) => {
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, 'dist')));
   
-  app.get('*', (req, res) => {
+  app.use((req, res) => {
     res.sendFile(path.join(__dirname, 'dist', 'index.html'));
   });
 }
